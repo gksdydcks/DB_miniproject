52ca407bf9344da5f8eca484a35b8091813cc95f
gksdydcks <dydcks1001@gmail.com>
2025-04-11 11:58:16 +0900

    fix last

--- main_client.cpp
+++ main_client.cpp
@@ -21,24 +21,27 @@ std::wstring utf8ToWstring(const std::string& str) {
     return conv.from_bytes(str);
 }
 
+SOCKET connectToServer(SOCKADDR_IN& serverAddr) {
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+    return clientSocket;
+}
+
 int main() {
     system("chcp 65001 > nul");
     _setmode(_fileno(stdin), _O_U16TEXT);
     _setmode(_fileno(stdout), _O_U16TEXT);
 
     WSADATA wsaData;
-    SOCKET clientSocket;
-    SOCKADDR_IN serverAddr;
-    std::vector<char> buffer(2048);
-
     WSAStartup(MAKEWORD(2, 2), &wsaData);
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
+    SOCKADDR_IN serverAddr{};
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(9000);
     inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 
-    connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+    std::vector<char> buffer(2048);
+    SOCKET clientSocket = connectToServer(serverAddr);
     std::wcout << L"서버에 연결되었습니다.\n";
 
     bool running = true;
@@ -75,79 +78,54 @@ int main() {
         }
 
         send(clientSocket, message.c_str(), static_cast<int>(message.length()), 0);
-
         int recvLen = recv(clientSocket, buffer.data(), static_cast<int>(buffer.size()), 0);
+
         if (recvLen > 0) {
-            try {
-                std::string response(buffer.data(), recvLen);
-                std::wcout << L"[서버 응답] " << utf8ToWstring(response) << L"\n";
+            std::string response(buffer.data(), recvLen);
+            std::wcout << L"[서버 응답] " << utf8ToWstring(response) << L"\n";
 
-                if (response == "Login Success") {
-                    bool login = true;
-                    while (login) {
-                        std::wcout << L"\n=== 로그인 후 메뉴 ===\n1. 채팅하기\n2. 로그아웃\n3. 종료\n> ";
-                        int sub;
-                        std::wcin >> sub;
-                        std::wcin.ignore();
+            if (response == "Login Success") {
+                bool login = true;
+                while (login) {
+                    std::wcout << L"\n=== 로그인 후 메뉴 ===\n1. 채팅하기\n2. 로그아웃\n3. 종료\n> ";
+                    int sub;
+                    std::wcin >> sub;
+                    std::wcin.ignore();
 
-                        if (sub == 1) {
-                            std::wcout << L"[채팅 모드] 'exit' 입력 시 채팅 종료\n";
-                            while (true) {
-                                std::wstring wmsg;
-                                std::getline(std::wcin, wmsg);
+                    if (sub == 1) {
+                        std::wcout << L"[채팅 모드] 'exit' 입력 시 채팅 종료\n";
+                        while (true) {
+                            std::wstring wmsg;
+                            std::getline(std::wcin, wmsg);
 
-                                if (wmsg == L"exit") break;
+                            if (wmsg == L"exit") break;
 
-                                std::string chatMsg = "CHAT:" + wideToUtf8(wmsg);
-                                send(clientSocket, chatMsg.c_str(), static_cast<int>(chatMsg.length()), 0);
+                            std::string chatMsg = "CHAT:" + wideToUtf8(wmsg);
+                            send(clientSocket, chatMsg.c_str(), static_cast<int>(chatMsg.length()), 0);
 
-                                int chatRecv = recv(clientSocket, buffer.data(), buffer.size(), 0);
-                                if (chatRecv > 0) {
-                                    std::string chatResp(buffer.data(), chatRecv);
-                                    std::wcout << L"[서버 응답] " << utf8ToWstring(chatResp) << L"\n";
-                                }
-                                else {
-                                    std::wcout << L"[서버 응답 수신 실패]\n";
-                                    break;
-                                }
+                            int chatRecv = recv(clientSocket, buffer.data(), buffer.size(), 0);
+                            if (chatRecv > 0) {
+                                std::string chatResp(buffer.data(), chatRecv);
+                                std::wcout << L"[서버 응답] " << utf8ToWstring(chatResp) << L"\n";
+                            }
+                            else {
+                                std::wcout << L"[서버 응답 수신 실패]\n";
+                                break;
                             }
                         }
-                        else if (sub == 2) {  // 로그아웃
-                            std::string logoutMsg = "exit";
-                            send(clientSocket, logoutMsg.c_str(), logoutMsg.length(), 0);
-
-                            int outRecv = recv(clientSocket, buffer.data(), buffer.size(), 0);
-                            if (outRecv > 0) {
-                                // 응답 무시하고 고정 메시지 출력
-                                std::wcout << L"[서버 응답] 로그아웃\n";
-                            }
-
-                            login = false;
-                        }
-                        else if (sub == 3) {  // 완전 종료
-                            std::string logoutMsg = "exit";
-                            send(clientSocket, logoutMsg.c_str(), logoutMsg.length(), 0);
-                            int outRecv = recv(clientSocket, buffer.data(), buffer.size(), 0);
-                            if (outRecv > 0) {
-                                std::wcout << L"[서버 응답] 로그아웃\n";
-                            }
-                            login = false;
-                            running = false;
-                            break;
-                        }
-                        else {
-                            std::wcout << L"잘못된 선택입니다.\n";
-                        }
+                    }
+                    else if (sub == 2 || sub == 3) {
+                        std::string logoutMsg = "exit";
+                        send(clientSocket, logoutMsg.c_str(), static_cast<int>(logoutMsg.length()), 0);
+                        recv(clientSocket, buffer.data(), buffer.size(), 0);
+                        std::wcout << L"[서버 응답] 로그아웃\n";
+                        closesocket(clientSocket);  // 소켓 종료
+                        clientSocket = connectToServer(serverAddr);  // 소켓 재연결
+                        login = false;
+                        if (sub == 3) running = false;
                     }
                 }
             }
-            catch (const std::exception& e) {
-                std::cerr << "[예외 발생] " << e.what() << std::endl;
-            }
-        }
-        else if (recvLen == 0) {
-            std::wcout << L"[서버 연결 종료]\n";
-            break;
         }
         else {
             std::wcout << L"[서버 응답 수신 실패]\n";
